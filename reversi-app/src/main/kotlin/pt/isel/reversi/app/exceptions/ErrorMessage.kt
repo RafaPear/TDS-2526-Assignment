b44c98ecfe05d4c16ab37cf40e63c868acaa99f2
ianfrunze <ian.frunze06@gmail.com>
2025-11-26 21:24:41 +0000

    feat: enhance ToastMessage with animated error display and improved message handling

--- reversi-app/src/main/kotlin/pt/isel/reversi/app/exceptions/ErrorMessage.kt
+++ reversi-app/src/main/kotlin/pt/isel/reversi/app/exceptions/ErrorMessage.kt
@@ -1,17 +1,22 @@
 package pt.isel.reversi.app.exceptions
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import pt.isel.reversi.app.state.AppState
@@ -32,18 +37,22 @@ fun ErrorMessage(appState: MutableState<AppState>, modifier: Modifier = Modifier
             LOGGER.info("${appState.value.error?.message}")
             ToastMessage(appState, modifier)
         }
+
         ErrorType.WARNING -> {
             LOGGER.warning("${appState.value.error?.message}")
             ToastMessage(appState, modifier)
         }
+
         ErrorType.ERROR -> {
             LOGGER.severe("${appState.value.error?.message}")
             ToastMessage(appState, modifier)
         }
+
         ErrorType.CRITICAL -> {
             LOGGER.severe("Critical ${appState.value.error?.message}")
             ToastMessage(appState, modifier)
         }
+
         null -> return
     }
 }
@@ -56,10 +65,14 @@ fun ErrorMessage(appState: MutableState<AppState>, modifier: Modifier = Modifier
  */
 @Composable
 fun ToastMessage(appState: MutableState<AppState>, modifier: Modifier = Modifier) {
+    val offsetY = remember { Animatable(-100f) }
+    val message = appState.value.error?.message
+    val durationMillis = 500L
     Box(
-        modifier = modifier
+        modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(16.dp)
+            .offset { IntOffset(x = 0, y = offsetY.value.toInt()) }, // posição animada
         contentAlignment = Alignment.TopCenter
     ) {
         Text(
@@ -71,10 +84,18 @@ fun ToastMessage(appState: MutableState<AppState>, modifier: Modifier = Modifier
         )
     }
 
-    LaunchedEffect(appState.value.error?.message) {
-        if (appState.value.error?.message == null) return@LaunchedEffect
-        // async wait for 2 seconds, more light than Thread.sleep
-        delay(2000)
+    LaunchedEffect(message) {
+        // slide in
+        offsetY.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(durationMillis = 300)
+        )
+        delay(durationMillis)
+        // slide out
+        offsetY.animateTo(
+            targetValue = -100f,
+            animationSpec = tween(durationMillis = 300)
+        )
         appState.value = setError(appState, error = null)
     }
 }
