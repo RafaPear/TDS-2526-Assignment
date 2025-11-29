311b51b1be062645d3222e2f115b96a0bdc660c8
ianfrunze <ian.frunze06@gmail.com>
2025-11-29 00:36:57 +0000

    feat: add ScaffoldView component for structured layout with top app bar and error handling

---
+++ reversi-app/src/main/kotlin/pt/isel/reversi/app/ScaffoldView.kt
@@ -0,0 +1,64 @@
+package pt.isel.reversi.app
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.TextAutoSize
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import pt.isel.reversi.app.exceptions.ErrorMessage
+import pt.isel.reversi.app.gamePage.TEXT_COLOR
+import pt.isel.reversi.app.state.AppState
+
+
+//**
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun ScaffoldView(
+    appState: MutableState<AppState>,
+    modifier: Modifier = Modifier,
+    title: String,
+    previousPageContent: @Composable () -> Unit,
+    content: @Composable () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent,
+                ),
+                title = {
+                    Text(
+                        text = title,
+                        color = TEXT_COLOR,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        autoSize = TextAutoSize.StepBased(
+                            maxFontSize = 50.sp
+                        ),
+                        maxLines = 1,
+                        softWrap = false,
+                    )
+                },
+                navigationIcon = {
+                    previousPageContent()
+                },
+            )
+        },
+        snackbarHost = { appState.value.error?.let { ErrorMessage(appState) } }
+    ) { padding ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            content()
+        }
+    }
+}
